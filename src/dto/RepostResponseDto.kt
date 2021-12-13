21e02f0729e2cbb80015f74f0f94096f1e210396
Danka02 <kamikoto02@mail.ru>
2021-12-13 15:59:30 +0300

    version 2

--- src/dto/RepostResponseDto.kt
+++ src/dto/RepostResponseDto.kt
@@ -5,7 +5,7 @@ import com.kuzmin.Model.RepostModel
 
 data class RepostResponseDto( val id: Long = 0,
                               val authorRepost: String? = null,
-                              val txtRepost: String? = null,) {
+                              val txtRepost: String? = null) {
     companion object {
         fun fromRepostModel(model: RepostModel) = RepostResponseDto(
             id = model.id,
